54e7a401444e6e78a3b2b6321672b5071cf69e9a
Sahilkumar26244 <100782217+Sahilkumar26244@users.noreply.github.com>
2022-05-21 11:42:01 +0530

    first

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Com from './unit-4/spint-1/day-1/assignment/components/Com';
+
+function App() {
+  return (
+    <div className="App">
+    <Com/>
+    </div>
+  );
+}
+
+export default App;
